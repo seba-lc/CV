c6aef96ad40fff0aa013730de07a7e8b1acb7278
Sebastian Lopez Cruz <sebalopezx@gmail.com>
2022-04-22 16:56:46 -0300

    cambio en el resumen

--- src/components/common/Resume.js
+++ src/components/common/Resume.js
@@ -1,7 +1,7 @@
 const Resume = () => {
   return (
     <div className='col-12 col-lg-6 col-xl-12'>
-      <p className='text-center lh-sm my-2 fs-7'>En el 2021 decidí perseguir lo que me gusta, decidí ahondar en el desarrollo web. Enamorado de la profesión, muy entusiasmado y motivado por abrirme paso en este maravilloso mundo.</p>
+      <p className='text-center lh-sm my-2 fs-7'>Enamorado de la profesión, muy entusiasmado y motivado por abrirme paso en este maravilloso mundo.</p>
       <p className='text-center lh-sm my-2 fs-7'>Actualmente busco la posibilidad de incorporarme como Desarrollador Junior. Soy una persona muy entusiasta, autodidacta y comprometida en mis responsabilidades.</p>
     </div>
   );
